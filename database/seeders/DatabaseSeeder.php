7a76b17a22a17befad29a7f092b0e8ba44aa93eb
Stephen Abrokwah <AbrokwahS@gmail.com>
2023-09-26 23:41:58 -0400

    added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Support\Collection;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +17,63 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        //roles
+        $registered_user = Role::create(['name' => 'registered user']);
+        $admin = Role::create(['name' => 'admin']);
+        $super_admin = Role::create(['name' => 'super admin']);
+        //permissions
+        $permissions = [
+            'create calendar event' => Permission::create(['name' => 'create calendar event']),
+            'edit calendar event' => Permission::create(['name' => 'edit calendar event']),
+            'delete calendar event' => Permission::create(['name' => 'delete calendar event']),
+            'read calendar event' => Permission::create(['name' => 'read calendar event']),
+            'create comment' => Permission::create(['name' => 'create comment']),
+            'edit comment' => Permission::create(['name' => 'edit comment']),
+            'delete comment' => Permission::create(['name' => 'delete comment']),
+            'read comment' => Permission::create(['name' => 'read comment']),
+            'register user' => Permission::create(['name' => 'register user']),
+            'register admin' => Permission::create(['name' => 'register admin']),
+            'edit user' => Permission::create(['name' => 'edit user']),
+            'edit admin' => Permission::create(['name' => 'edit admin']),
+            'ban admin' => Permission::create(['name' => 'ban admin']),
+            'ban user' => Permission::create(['name' => 'ban user']),
+        ];
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $permissions['read calendar event']->assignRole($registered_user);
+        $permissions['read calendar event']->assignRole($admin);
+        $permissions['create calendar event']->assignRole($admin);
+        $permissions['edit calendar event']->assignRole($admin);
+        $permissions['delete calendar event']->assignRole($admin);
+
+        $permissions['create comment']->assignRole($registered_user);
+        $permissions['read comment']->assignRole($registered_user);
+        $permissions['edit comment']->assignRole($registered_user);
+        $permissions['delete comment']->assignRole($registered_user);
+
+        $permissions['create comment']->assignRole($admin);
+        $permissions['read comment']->assignRole($admin);
+        $permissions['edit comment']->assignRole($admin);
+        $permissions['delete comment']->assignRole($admin);
+
+        $permissions['register user']->assignRole($admin);
+        $permissions['edit user']->assignRole($registered_user);
+
+        for ($c = 0; $c < 12; $c++) {
+            $user = User::create([
+                'name' => fake()->name(),
+                'email' => fake()->unique()->safeEmail(),
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+            ]);
+
+            if ($c == 0) {
+                $user->assignRole($super_admin);
+            } else if ($c == 1) {
+                $user->assignRole($admin);
+            } else {
+                $user->assignRole($registered_user);
+            }
+        }
     }
 }
